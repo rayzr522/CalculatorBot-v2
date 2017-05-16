079e07e8175b692a458599473f6aa1328b08df33
Rayzr522 <rayzr522@gmail.com>
2017-05-16 15:29:07 -0400

    Added response mappings

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ const Mexp = require('math-expression-evaluator')
 const bot = new Discord.Client()
 const config = bot.config = require('dotenv').config().parsed
 
+const mappedResponses = {
+    42: 'Life',
+    666: ':smiling_imp:',
+    me: msg => msg.author
+}
+
+const latestAnswers = {}
+
+function getResponse(msg, input) {
+    var handler = mappedResponses[input]
+    if (!handler) {
+        return input
+    }
+
+    if (typeof handler === 'function') {
+        return handler(msg)
+    }
+    return handler.toString()
+}
+
 bot.on('ready', () => {
     bot.user.setAvatar('./avatar.png')
     bot.user.setGame(`${config.prefix}calc`)
@@ -13,7 +33,7 @@ bot.on('ready', () => {
 })
 
 bot.on('message', msg => {
-    if (msg.author.bot) {
+    if (msg.author.bot || !msg.content.startsWith(config.prefix)) {
         return
     }
 
@@ -23,9 +43,19 @@ bot.on('message', msg => {
 
     if (/^calc(ulate)?$/.test(command)) {
         try {
-            msg.reply(Mexp.eval(args.join(' ')))
+            var input = args.join(' ')
+            var output = getResponse(msg, input)
+            if (output === input) {
+                if (latestAnswers[msg.author.id] !== undefined) {
+                    input = input.replace(/ans(wer)?/gi, latestAnswers[msg.author.id])
+                }
+                output = getResponse(msg, Mexp.eval(input))
+                latestAnswers[msg.author.id] = output
+            }
+
+            msg.channel.sendMessage(`:crystal_ball: __${output}__`)
         } catch (err) {
-            msg.channel.sendMessage(`:x: ${err.message || err}`).then(m => m.delete(5000))
+            msg.channel.sendMessage(`:x: \`${err.message || err}\``).then(m => m.delete(5000))
         }
     } else if (command === 'ping') {
         msg.channel.sendMessage('Ping!').then(m => m.edit(`:stopwatch: Pong! \`${m.createdTimestamp - msg.createdTimestamp}ms\``))
